cc05839ca5726f22575aea3ba00acb34b561f902
Belenko Vladimir <vbelenko@ntdev.ru>
2025-07-23 15:28:49 +0300

    [fix] импортов

--- src/shared/lib/langchain/tools/index.ts
+++ src/shared/lib/langchain/tools/index.ts
@@ -2,6 +2,12 @@
  * Экспорты всех LangChain инструментов
  */
 
+// Импортируем модули с инструментами. Используем явные импорты,
+// чтобы избежать использования `require` и сохранить поддержку ESM.
+import * as sceneTools from './sceneTools'
+import * as instanceTools from './instanceTools'
+import * as objectTools from './objectTools'
+
 // Scene information tools
 export {
   getSceneObjectsTool,
@@ -22,26 +28,22 @@ export {
 } from './objectTools'
 
 /**
- * Функция для получения всех доступных инструментов сцены
- * Используется для избежания проблем с circular imports
+ * Функция для получения всех доступных инструментов сцены.
+ * Благодаря статическим импортам исключаются проблемы с `require`
+ * и поддерживается работа в модуле ES.
  */
 export function getAllSceneTools() {
-  // Импортируем инструменты динамически
-  const sceneTools = require('./sceneTools')
-  const instanceTools = require('./instanceTools')  
-  const objectTools = require('./objectTools')
-
   return [
     // Информационные инструменты
     sceneTools.getSceneObjectsTool,
     sceneTools.getSceneStatsTool,
     sceneTools.findObjectByNameTool,
-    
+
     // Инструменты управления экземплярами
     instanceTools.addObjectInstanceTool,
     instanceTools.canAddInstanceTool,
     instanceTools.getObjectInstancesTool,
-    
+
     // Инструменты создания объектов
     objectTools.addNewObjectTool
   ]
