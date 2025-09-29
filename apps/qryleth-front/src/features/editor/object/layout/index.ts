e7eb383f21607b3abe3a7f69ba60df132799d7a1
Your Name <you@example.com>
2025-09-29 14:44:38 +0300

    Удален неиспользуемый код

--- apps/qryleth-front/src/features/editor/object/layout/index.ts
+++
@@ -1,4 +0,0 @@
-// Публичные точки входа layout‑фичи ObjectEditor
-export * from './hooks/useObjectPanelLayout'
-export * from './model/panelLayoutStore'
-export * from './model/panelVisibilityStore'
--- apps/qryleth-front/src/features/editor/scene/index.ts
+++
@@ -1,5 +0,0 @@
-// Публичное API scene-редактора в пространстве editor/scene
-// Экспортируем основные подфичи: layout и toolbar
-export * from './layout'
-export * from './toolbar'
-
